5bef157f43a4846321cf57c1aad76a6d931d4b9a
Vakho <yoshida12ko12@gmail.com>
2025-05-17 09:11:25 -0700

    basuc events +1

--- SoolraConsole/Model/EngagementTracker.swift
+++ SoolraConsole/Model/EngagementTracker.swift
@@ -11,10 +11,10 @@ import FirebaseAnalytics
 class EngagementTracker: ObservableObject {
     private var timer: Timer?
     private var currentRom: String = "none"
-
+    
     func startTracking() {
         stopTracking() // stop any previous timer
-
+        
         timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { _ in
             Analytics.logEvent("session_heartbeat", parameters: [
                 "current_rom": self.currentRom,
@@ -22,7 +22,7 @@ class EngagementTracker: ObservableObject {
             ])
         }
     }
-
+    
     func stopTracking() {
         timer?.invalidate()
         timer = nil
@@ -30,5 +30,9 @@ class EngagementTracker: ObservableObject {
     
     func setCurrentRom(_ rom: String) {
         self.currentRom = rom
+        Analytics.logEvent("game_started", parameters: [
+            "current_rom": self.currentRom,
+            "timestamp": Date().timeIntervalSince1970
+        ])
     }
 }
